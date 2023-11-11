444037c3763987a807c5666bde14c7b8a025c07c
T0MYAMMM <18220106@std.stei.itb.ac.id>
2023-11-11 09:14:48 +0700

    task 4 completed with precondition

--- proxyServer.js
+++ proxyServer.js
@@ -1,47 +1,109 @@
 const http = require('http');
+const https = require('https');
 const net = require('net'); 
 const url = require('url');
+const fs = require('fs');
+//const { createCertificate } = require('pem'); // Menggunakan pem untuk pembuatan sertifikat dinamis
+
+// SSL/TLS options untuk server intersepsi
+const options = {
+    key: fs.readFileSync('rootCA-key.pem'), // Private Key SSL/TLS
+    cert: fs.readFileSync('rootCA-cert.pem') // Public Certificate SSL/TLS
+};
+
+// Membuat server HTTPS untuk intersepsi
+const sslServer = https.createServer(options, (req, res) => {
+    // Logika untuk memodifikasi isi bisa ditempatkan di sini
+    const requestOptions = {
+        hostname: req.headers['host'],
+        port: 443,
+        path: req.url,
+        method: req.method,
+        headers: req.headers,
+    };
+
+    // Membuat request ke server 
+    const proxyReq = https.request(requestOptions, (proxyRes) => {
+        let chunks = [];
+
+        proxyRes.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        proxyRes.on('end', () => {
+            let body = Buffer.concat(chunks);
+            let bodyString = body.toString();
+
+            if (bodyString.toLowerCase().includes('html')) {
+                bodyString += 'NODEJS'
+                console.log('HTML substring found and modified');
+            } else {
+                console.log('HTML substring is missing!');
+            }
+
+            // Update the content-length
+            const updatedHeaders = {
+                ...proxyRes.headers,
+                'content-length' : Buffer.byteLength(bodyString),
+            };
+
+            res.writeHead(proxyRes.statusCode, updatedHeaders);
+            res.end(bodyString);
+        });
+    });
+    proxyReq.on('error', (err) => {
+        console.error('HTTPS request error:', err.message);
+        res.writeHead(500);
+        res.end('Proxy encountered an error');
+    });
+    req.pipe(proxyReq);
+});
+
+sslServer.listen(3457, () => {
+    console.log('SSL interception server is running on https://localhost:3457');
+});
 
 const server = http.createServer((req, res) => {
+    // HTTP
     const parsedUrl = url.parse(req.url);
-
+    
     console.log('Received request for:', req.url);
 
     // Forward HTTP requests
-    if (parsedUrl.protocol === 'http:') {
+    if (parsedUrl.protocol === 'http:'){
         const options = {
-            hostname: parsedUrl.hostname,
+            hostname : parsedUrl.hostname,
             port: parsedUrl.port || 80,
-            path: parsedUrl.path, 
+            path: parsedUrl.path,
             method: req.method,
             headers: req.headers
         };
 
         const proxyReq = http.request(options, (proxyRes) => {
             let chunks = [];
-        
+
             proxyRes.on('data', (chunk) => {
                 chunks.push(chunk);
             });
-        
+
             proxyRes.on('end', () => {
                 let body = Buffer.concat(chunks);
                 let bodyString = body.toString();
-        
+
                 if (bodyString.toLowerCase().includes('html')) {
                     bodyString += 'NODEJS';
-                    console.log('HTML substring found and modified.');
+                    console.log('HTML substring found and modified');
                 } else {
                     console.log('HTML substring is missing!');
                 }
-                    
-                    // Update the content-length
-                const updatedHeaders = {
+
+                // Update the content-length
+                const updateHeaders = {
                     ...proxyRes.headers,
-                    'content-length': Buffer.byteLength(bodyString)
-                };        
-                res.writeHead(proxyRes.statusCode, updatedHeaders);
-                res.end(bodyString);
+                    'content-length' : Buffer.byteLength(bodyString)
+                };
+                res.writeHead(proxyRes.statusCode, updateHeaders);
+                res.end(bodyString)
             });
         });
 
@@ -55,20 +117,18 @@ const server = http.createServer((req, res) => {
 });
 
 server.on('connect', (req, cltSocket, head) => {
-    console.log('Received CONNECT request for:', req.url);
+    console.log('Received CONNECT request for:', req.url)
+  
+    // Logika untuk HTTPS
 
-    // Parsing hostname dan port dari request
-    const [hostname, port] = req.url.split(':');
-
-    // Menangani request HTTPS
-    const proxySocket = net.connect(port || 443, hostname, () => {
-        console.log('TCP connection established with', hostname);
+    const proxySocket = net.connect(3457, 'localhost', () => {
+        // Meneruskan ke server intersepsi SSL/TLS
         cltSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
         proxySocket.write(head);
         proxySocket.pipe(cltSocket);
         cltSocket.pipe(proxySocket);
     });
-
+    
     proxySocket.on('error', (err) => {
         console.error('TCP connection error:', err.message);
     });
